cb14fc802b3dd43c98ea427b468e0d07210fabc3
KP <krishna.prasad.srinivasan@philips.com>
2022-01-12 10:29:06 +0530

    intermediate still not working

--- restOne/index.ts
+++ restOne/index.ts
@@ -3,7 +3,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 import jwt = require('jsonwebtoken');
 import jwksClient = require('jwks-rsa');
-const DISCOVERY_KEYS_ENDPOINT = "https://login.microsoftonline.com/1a407a2d-7675-4d17-8692-b3ac285306e4/discovery/v2.0/keys";
+const DISCOVERY_KEYS_ENDPOINT = "https://login.microsoftonline.com/common/discovery/keys";
 
 
 const validateJwt = async (req, context) => {
@@ -28,29 +28,30 @@ const validateJwt = async (req, context) => {
     }
 };
 
-const getSigningKeys = (header, callback) => {
+const getSigningKeys = (header) => {
     var client = jwksClient({
         jwksUri: DISCOVERY_KEYS_ENDPOINT
     });
 
     client.getSigningKey(header.kid, function (err, key) {
-        var signingKey = key.publicKey || key.rsaPublicKey;
-        callback(null, signingKey);
+
+        var signingKey = (key as jwksClient.CertSigningKey).publicKey || (key as jwksClient.RsaSigningKey).rsaPublicKey;
+        return signingKey;
     });
 }
 
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const x =  validateJwt(req, context);
+    const x =  await validateJwt(req, context);
     context.log("validation respose is :" + x);
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully. changed by kp"
+        ? "Hello, " + name + ". validation response was:"+ x +". This HTTP triggered function executed successfully. changed by kp"
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.log(JSON.stringify(context, null, 2));
-    context.log(JSON.stringify(req.headers, null, 2));
+    // context.log(JSON.stringify(context, null, 2));
+    // context.log(JSON.stringify(req.headers, null, 2));
 
     context.res = {
         // status: 200, /* Defaults to 200 */
